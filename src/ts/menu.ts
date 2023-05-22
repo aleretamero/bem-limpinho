bbfeeaea9f7ba307e51b43218f38749d1774f1b7
Alexandre Retamero <alexandreretamero@gmail.com>
2023-05-22 16:56:03 -0300

    Create menu.ts

---
+++ src/ts/menu.ts
@@ -0,0 +1,41 @@
+const navbar = document.querySelector('.navbar');
+const buttonMenu = document.querySelector('.button-menu');
+const optionsMenu = document.querySelector('.options-menu');
+
+if (!optionsMenu || !buttonMenu || !navbar) {
+  throw new Error('Não foi possivel encontra os elementos do menu!');
+}
+
+const options = optionsMenu.querySelectorAll('li');
+
+options.forEach(option => {
+  option.addEventListener('click', () => {
+    if (
+      buttonMenu.classList.contains('active') &&
+      optionsMenu.classList.contains('active')
+    ) {
+      buttonMenu.classList.remove('active');
+      optionsMenu.classList.remove('active');
+    }
+  });
+});
+
+export const menu = () => {
+  buttonMenu.addEventListener('click', () => {
+    buttonMenu.classList.toggle('active');
+    optionsMenu.classList.toggle('active');
+  });
+
+  document.addEventListener('DOMContentLoaded', menuMobile);
+  window.addEventListener('resize', menuMobile);
+};
+
+const menuMobile = () => {
+  if (window.innerWidth <= 768) {
+    navbar.classList.add('mobile');
+    optionsMenu.classList.add('mobile');
+  } else {
+    navbar.classList.remove('mobile');
+    optionsMenu.classList.remove('mobile');
+  }
+};
