e0d44f67c10ce85caea676936c5f81bc9714f5b0
Alexandre Retamero <alexandreretamero@gmail.com>
2023-05-24 12:38:36 -0300

    Create carousel.ts

---
+++ src/ts/carousel.ts
@@ -0,0 +1,59 @@
+const imgPrev = document.querySelector('.photo.blur:first-child');
+const imgNext = document.querySelector('.photo.blur:last-child');
+const imgEmphasis = document.querySelector('.photo.emphasis');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+const imgs = [
+  './assets/imgs-carousel/cadeiras.jpg',
+  './assets/imgs-carousel/quarto.jpg',
+  './assets/imgs-carousel/sofa.jpg',
+  './assets/imgs-carousel/tapete.jpg',
+];
+
+function loadGallery(index = 0) {
+  
+  imgEmphasis?.setAttribute('src', imgs[index]);
+
+  if (index === 0) {
+    imgPrev?.setAttribute('src', imgs[imgs.length - 1]);
+  } else {
+    imgPrev?.setAttribute('src', imgs[index - 1]);
+  }
+
+  if (index === imgs.length - 1) {
+    imgNext?.setAttribute('src', imgs[0]);
+  } else {
+    imgNext?.setAttribute('src', imgs[index + 1]);
+  }
+}
+
+let index: number = 0;
+
+const upIndex = () => {
+  if (index === imgs.length - 1) {
+    index = 0;
+  } else {
+    index++;
+  }
+  loadGallery(index);
+};
+
+const bellowIndex = () => {
+  if (index === 0) {
+    index = imgs.length - 1;
+  } else {
+    index--;
+  }
+  loadGallery(index);
+};
+
+export function carousel() {
+  prev?.addEventListener('click', bellowIndex);
+  next?.addEventListener('click', upIndex);
+
+  imgPrev?.addEventListener('click', bellowIndex);
+  imgNext?.addEventListener('click', upIndex);
+
+  loadGallery(index);
+}
