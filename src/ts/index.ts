41f8ca357e9859690cee956cde73af27e909ecf7
Alexandre Retamero <alexandreretamero@gmail.com>
2023-05-24 12:38:38 -0300

    Update index.ts

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -3,10 +3,12 @@ import VanillaTilt from 'vanilla-tilt';
 import { menu } from './menu';
 import { scrollReveal } from './scrollReveal';
 import { observeSection } from './couter';
+import {carousel} from './carousel'
 
 menu();
 scrollReveal();
 observeSection();
+carousel()
 
 const cardsBenefits: NodeListOf<HTMLElement> | null =
   document.querySelectorAll('.card-benefits');
